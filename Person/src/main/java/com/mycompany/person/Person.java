2434b6bc6e18e6e34d3aea388839deb63ed5f1eb
Chanodya Praveen Tapassarage <135603936+Chanodyapraveen@users.noreply.github.com>
2024-10-06 23:07:47 +0530

    Person

---
+++ Person/src/main/java/com/mycompany/person/Person.java
@@ -0,0 +1,15 @@
+
+package com.mycompany.person;
+public class Person {
+
+    public static void main(String[] args) 
+    {
+     PersonObj P1 = new PersonObj("praveen",21);
+     PersonObj P2 = new PersonObj("chanodya",21);
+     
+     P1.display();
+     P2.display();
+             
+    }
+    
+}
---
+++ Person/src/main/java/com/mycompany/person/PersonObj.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.person;
+
+/**
+ *
+ * @author asus
+ */
+public class PersonObj 
+{
+ public String name;
+ public  int age;
+ 
+ 
+ // constructor
+ public PersonObj (String name, int age )
+ {
+  this.name=name;
+  this.age=age;
+  }
+ 
+ public void display()
+ {
+     System.out.println("The name is + praveen");
+     System.out.println("The age is + chanodya");
+ }
+}
